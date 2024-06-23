fb3ea842b3aa703d4c9c05aeb93b010f4c9cbbd8
DamirTesnjak <damir.tesnjak@gmail.com>
2024-06-23 20:48:22 +0200

    Updated packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  ...fixupConfigRules(pluginReactConfig),
+  pluginReactConfig,
 ];
